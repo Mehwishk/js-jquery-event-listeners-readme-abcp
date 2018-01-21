96f6dc9f4523b1ea1a0939f49f76d0eceb959276
Mehwish Kapadia <mehwishkap@gmail.com>
2018-01-21 18:34:01 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,4 +1,27 @@
 //define functions here
+function getIt()
+$('p').on("click", function()
+{
+  alert ("Hey!")
+})
+
+function frameIt()
+$('.tasty').on('"load', function()
+{
+  img: 
+})
+
+function pressIt()
+$('form').on ('keydown', function(key){
+  if (key.which == 71){
+    alert('G was pressed');
+  }
+})
+function submitIt()
+$('form').on('submit', function(){
+  alert ('Your form is going to be submitted now');
+})
+
 
 $(document).ready(function(){
 
