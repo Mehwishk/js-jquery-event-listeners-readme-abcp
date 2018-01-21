d20eaf8c94898ab41d8480c62a3d277d04af74bc
Mehwish Kapadia <mehwishkap@gmail.com>
2018-01-21 18:46:31 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -5,7 +5,7 @@ function getIt(){
 $('p').on('click', function()
 {
   alert ('Hey!');
-})
+});
 }
 
 function frameIt(){
@@ -20,12 +20,12 @@ $('input').on ('keydown', function(key){
   if (key.which == 71){
     alert('G was pressed');
   }
-})
+});
 }
 function submitIt(){
 $('form').on('submit', function(){
   alert ('Your form is going to be submitted now');
-})
+});
 }
 
 $(document).ready(function(){
